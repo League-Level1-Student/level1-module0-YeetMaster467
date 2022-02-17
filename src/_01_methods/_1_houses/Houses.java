fef9801a5d80912c419bcfd42f543621930d77b9
YeetMaster467 <elicomixinc@gmail.com>
2022-02-16 19:59:37 -0800

    Houses in progress

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,4 +1,7 @@
 package _01_methods._1_houses;
+
+import org.jointheleague.graphical.robot.Robot;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
@@ -7,5 +10,13 @@ public class Houses {
 		
 	public void run() {
 		// Check the recipe to find out what code to put here
+		Robot rob = new Robot("mini");
+		rob.moveTo(25, 500);
+		
+		rob.penDown();
+		rob.move(100);
+		rob.turn(90);
+		rob.move(50);
+		// rob.turn();
 	}
 }

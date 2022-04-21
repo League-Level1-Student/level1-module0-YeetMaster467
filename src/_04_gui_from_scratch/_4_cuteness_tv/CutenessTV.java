6335944e36b6c7f387dca2559c323c84b03d829f
YeetMaster467 <elicomixinc@gmail.com>
2022-04-20 19:14:13 -0700

    Completed Cuteness TV

--- src/_04_gui_from_scratch/_4_cuteness_tv/CutenessTV.java
+++ src/_04_gui_from_scratch/_4_cuteness_tv/CutenessTV.java
@@ -1,10 +1,14 @@
 package _04_gui_from_scratch._4_cuteness_tv;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URI;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class CutenessTV {
+public class CutenessTV implements ActionListener {
 	
 	JButton button1 = new JButton();
 	JButton button2 = new JButton();
@@ -25,6 +29,43 @@ public class CutenessTV {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		panel.setVisible(true);
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		button3.addActionListener(this);
 		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton buttonPressed = (JButton) e.getSource();
+		
+		if(buttonPressed.equals(button1)) {
+			showDucks();
+		}else if(buttonPressed.equals(button2)) {
+			showFrog();
+		}else if(buttonPressed.equals(button3)) {
+			showFluffyUnicorns();
+		}
+	}
+	
+	void showDucks() {
+		playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
+	}
+	
+	void showFrog() {
+		playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+	}
+	
+	void showFluffyUnicorns() {
+		playVideo("https://www.youtube.com/watch?v=a-xWhG4UU_Y");
+	}
+	
+	void playVideo(String videoID) {
+		try {
+			URI uri = new URI(videoID);
+			java.awt.Desktop.getDesktop().browse(uri);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
